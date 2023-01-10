bc7af5b994b323fb01b02457d4cde115fcfa188b
macsoftware <87543117+macsoftware@users.noreply.github.com>
2023-01-10 12:14:26 +0000

    Added the backend code to the UI so that it does some simple calculations. There is very minimal error handling so divide by zero will crash the app just now.

--- VerySimpleCalculator/ViewController.swift
+++ VerySimpleCalculator/ViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func additionClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstTextField.text!){
+            if let secondNumber = Int(secondTextField.text!){
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }else{
+                resultLabel.text = "Second number needs to be an integer"
+            }
+        }else{
+            resultLabel.text = "First number needs to be an integer"
+        }
+    }
+    
+    @IBAction func subtractionClicked(_ sender: Any) {
+        if let firstNumber = Int(firstTextField.text!){
+            if let secondNumber = Int(secondTextField.text!){
+                result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }else{
+                resultLabel.text = "Second number needs to be an integer"
+            }
+        }else{
+            resultLabel.text = "First number needs to be an integer"
+        }
+    }
+    
+    @IBAction func multiplicationClicked(_ sender: Any) {
+        if let firstNumber = Int(firstTextField.text!){
+            if let secondNumber = Int(secondTextField.text!){
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }else{
+                resultLabel.text = "Second number needs to be an integer"
+            }
+        }else{
+            resultLabel.text = "First number needs to be an integer"
+        }
+    }
+    
+    @IBAction func divisionClicked(_ sender: Any) {
+        if let firstNumber = Int(firstTextField.text!){
+            if let secondNumber = Int(secondTextField.text!){
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }else{
+                resultLabel.text = "Second number needs to be an integer"
+            }
+        }else{
+            resultLabel.text = "First number needs to be an integer"
+        }
+    }
+    
 }
 
